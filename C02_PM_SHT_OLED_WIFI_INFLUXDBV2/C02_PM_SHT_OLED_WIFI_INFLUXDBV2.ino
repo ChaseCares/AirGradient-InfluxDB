ffb07e3f6ca512a584072c221e8641b453e39b89
Chase C <13863948+ChaseCares@users.noreply.github.com>
2021-11-09 06:31:17 -0500

    multitasking! decoupled display and database

--- C02_PM_SHT_OLED_WIFI_INFLUXDBV2/C02_PM_SHT_OLED_WIFI_INFLUXDBV2.ino
+++ C02_PM_SHT_OLED_WIFI_INFLUXDBV2/C02_PM_SHT_OLED_WIFI_INFLUXDBV2.ino
@@ -26,14 +26,16 @@ MIT License
 
 ---modifications---
 
-The original example code was modified to report data directly to a local influxDB v2 server.
-Other changes in features:
-  - Removed captive portal, Wi-Fi credentials are now defined below
-  - data is now sent to influxDB
-  - added the ability to display Fahrenheit on display, celsius is still reported to the database ()
-  - added a calibration offset for temperature (as Jeff Gerling noted temperatures are usually a bit high)
-      the calibration value is subtracted from the reading
+The original example code was modified to add additional features.
+	Changes:
+	- removed captive portal, Wi-Fi credentials are now defined below
+	- data is can be sent to influxDB
+	- added the ability to display Fahrenheit on display, celsius is still reported to the database
+	- added a calibration offset for temperature (as Jeff Gerling noted temperatures are usually a bit high)
+		the calibration value is subtracted from the reading
+	- added the ability to update the database and the display independently
 */
+
 
 // ------------------------------------------start config-----------------------------------------------
 
@@ -65,19 +67,25 @@ String PASSWORD = "REPLACE_WITH_YOUR_WI-FI_PASSWORD";
 #define HAS_CO2 true
 #define HAS_SHT true
 
-// delay for how long each reading is displayed on the screen in milliseconds
-int DELAY = 6000;
+// time in seconds to add data to the database. must be less than 24 days
+const unsigned int ADD_TO_DATABASE_INTERVAL = 20;
+
+// time in seconds to update the display. must be less than 24 days
+const unsigned int DISPLAY_INTERVAL = 30;
+
+// write data to the database
+#define DATABASE_EXPORT true
 
 // true display is Fahrenheit on the display, false displays in Celsius
-#define FAHRRENHEIT false
+#define FAHRENHEIT false
 
-// an amount to subtract from the temperature reading, i found 2 °C to be a good sweet spot
+// amount to subtract from the temperature reading, i found 2 °C to be a good sweet spot
 float TEMP_OFFSET = 2.0;
 
-// Set timezone string according to https://www.gnu.org/software/libc/manual/html_node/TZ-Variable.html
-// Examples: Pacific Time "PST8PDT", Japanesse "JST-9", Central Europe "CET-1CEST,M3.5.0,M10.5.0/3"
+// set timezone string according to https://www.gnu.org/software/libc/manual/html_node/TZ-Variable.html
+// examples: Pacific Time "PST8PDT", Japanesse "JST-9", Central Europe "CET-1CEST,M3.5.0,M10.5.0/3"
 
-// Currently set to Eastern standard Time
+// currently set to Eastern standard Time
 #define TZ_INFO "EST+5EDT,M3.2.0/2,M11.1.0/2"
 
 // use the default or the IP address of your local NTP server
@@ -90,6 +98,7 @@ float TEMP_OFFSET = 2.0;
 #include <ESP8266WiFi.h>
 #include <SSD1306Wire.h>
 #include <AirGradient.h>
+#include <iterator>
 #include <Wire.h>
 
 // data point
@@ -102,92 +111,181 @@ SSD1306Wire display(0x3c, SDA, SCL);
 // InfluxDB client instance with preconfigured InfluxCloud certificate
 InfluxDBClient client(INFLUXDB_URL, INFLUXDB_ORG, INFLUXDB_BUCKET, INFLUXDB_TOKEN, InfluxDbCloud2CACert);
 
+unsigned long elapsedMs = 0;
+const unsigned int MS = 1000;
+
+void writeToDatabase();
+void displayPM();
+void displayCO2();
+void displayTemp();
+void displayHUM();
+
+struct Task {
+	void (*callback)();	// function to execute every time the timeout expires
+	long timeout; // how much time is left before the callback should be executed
+	unsigned long interval; // what to reset the timeout to once it expires
+};
+
+Task tasks[] = {
+	#if HAS_SHT
+	{displayTemp},
+	{displayHUM,},
+	#endif
+
+	#if HAS_PM
+	{displayPM,},
+	#endif
+
+	#if HAS_CO2
+	{displayCO2,},
+	#endif
+
+	#if DATABASE_EXPORT
+	{writeToDatabase,}
+	#endif
+};
+
+
 void setup() {
-  Serial.begin(9600);
+	Serial.begin(9600);
 
-  display.init();
-  display.flipScreenVertically();
-  showTextRectangle("(*^_^*)", "Starting", true, 500);
+	display.init();
+	display.flipScreenVertically();
+	showTextRectangle("(*^_^*)", "Starting", true);
 
-  if (HAS_PM) ag.PMS_Init();
-  if (HAS_CO2) ag.CO2_Init();
-  if (HAS_SHT) ag.TMP_RH_Init(0x44);
+	if (HAS_PM) ag.PMS_Init();
+	if (HAS_CO2) ag.CO2_Init();
+	if (HAS_SHT) ag.TMP_RH_Init(0x44);
 
-  // setup and wait for Wi-Fi
-  WiFi.begin(SSID, PASSWORD);
-  Serial.println("");
+	const int totalTasks = HAS_PM + HAS_CO2 + HAS_SHT * 2;
 
-  showTextRectangle("Setting", "up WiFi", true, 0);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  showTextRectangle("Host-", "name:", true, DELAY);
-  showTextRectangle(String(WiFi.hostname().substring(0, 4)), String(WiFi.hostname().substring(4, 10)), true, DELAY);
+	for (size_t i = 0; i < totalTasks; i++) {
+		tasks[i].timeout = DISPLAY_INTERVAL * MS * i;
+		tasks[i].interval = DISPLAY_INTERVAL * MS * totalTasks;
+	}
 
-  Serial.println("");
-  timeSync(TZ_INFO, NTP_SERVER);
+	if (DATABASE_EXPORT) {
+		tasks[totalTasks].timeout = ADD_TO_DATABASE_INTERVAL * MS;
+		tasks[totalTasks].interval = ADD_TO_DATABASE_INTERVAL * MS;
+	}
 
+	// setup and wait for Wi-Fi
+	WiFi.begin(SSID, PASSWORD);
+	Serial.println("");
 
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(SSID);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-  Serial.print("MAC address: ");
-  Serial.println(WiFi.macAddress());
-  Serial.print("Hostname: ");
-  Serial.println(String(WiFi.hostname()));
-}
+	showTextRectangle("Setting", "up WiFi", true);
+	while (WiFi.status() != WL_CONNECTED) {
+		delay(500);
+		Serial.print(".");
+	}
+
+	showTextRectangle("Host-", "name:", true);
+	showTextRectangle(String(WiFi.hostname().substring(0, 4)), String(WiFi.hostname().substring(4, 10)), true);
+
+	Serial.println("");
+	timeSync(TZ_INFO, NTP_SERVER);
+
+	Serial.println("");
+	Serial.print("Connected to ");
+	Serial.println(SSID);
+	Serial.print("IP address: ");
+	Serial.println(WiFi.localIP());
+	delay(5000);
+	Serial.print("MAC address: ");
+	Serial.println(WiFi.macAddress());
+	Serial.print("Hostname: ");
+	Serial.println(String(WiFi.hostname()));
+	delay(5000);
+
+	elapsedMs = millis();
+	}
+
 
 void loop() {
-  // Clear fields for reusing the point.
-  sensor.clearFields();
+	const unsigned long currentMillis0 = millis();
+	const unsigned long deltaMs = currentMillis0 - elapsedMs;
+	elapsedMs = currentMillis0;
 
-  if (HAS_PM) {
-    int PM2 = ag.getPM2_Raw();
-    sensor.addField("pm02", PM2);
-    showTextRectangle("PM2", String(PM2), false, DELAY);
-  }
-
-  if (HAS_CO2) {
-    int CO2 = ag.getCO2_Raw();
-      sensor.addField("co2", CO2);
-      showTextRectangle("CO2", String(CO2), false, DELAY);
-  }
-
-  if (HAS_SHT) {
-    TMP_RH result = ag.periodicFetchData();
-    float caliTemp = (result.t - TEMP_OFFSET);
-    sensor.addField("humidity", result.rh);
-    showTextRectangle("HMTY", String(result.rh) + "%", false, DELAY);
-
-    sensor.addField("temperature", caliTemp);
-    if (FAHRRENHEIT)
-      showTextRectangle("TEMP", String((caliTemp * 1.8) + 32.0, 1) + "°F", true, DELAY);
-    else
-      showTextRectangle("TEMP", String(caliTemp, 1) + "°C", true, DELAY);
-  }
-
-  // write point
-  if (!client.writePoint(sensor)) {
-    Serial.print("InfluxDB write failed: ");
-    Serial.println(client.getLastErrorMessage());
-  }
+	for (size_t i = 0; i < std::size(tasks); i++) {
+		tasks[i].timeout -= deltaMs;
+		if (tasks[i].timeout <= 0) {
+			tasks[i].callback();
+			tasks[i].timeout += tasks[i].interval;
+		}
+	}
 }
 
-// display
-void showTextRectangle(String LN1, String LN2, boolean SMALL_TXT, int DELAY)
-{
-  display.clear();
-  display.setTextAlignment(TEXT_ALIGN_LEFT);
-  if (SMALL_TXT)
-    display.setFont(ArialMT_Plain_16);
-  else
-    display.setFont(ArialMT_Plain_24);
 
-  display.drawString(32, 16, LN1);
-  display.drawString(32, 36, LN2);
-  display.display();
-  delay(DELAY);
+void writeToDatabase() {
+	// clear fields for reusing the point.
+	sensor.clearFields();
+
+	if (HAS_PM) {
+		int PM2 = ag.getPM2_Raw();
+		sensor.addField("pm02", PM2);
+	}
+
+	if (HAS_CO2) {
+		int CO2 = ag.getCO2_Raw();
+		sensor.addField("co2", CO2);
+	}
+
+	if (HAS_SHT) {
+		delay(80)
+		TMP_RH result = ag.periodicFetchData();
+		float caliTemp = (result.t - TEMP_OFFSET);
+		sensor.addField("humidity", result.rh);
+		sensor.addField("temperature", caliTemp);
+	}
+
+	// write point
+	if (!client.writePoint(sensor)) {
+		Serial.print("InfluxDB write failed: ");
+		Serial.println(client.getLastErrorMessage());
+	}
+}
+
+
+void displayPM() {
+	int PM2 = ag.getPM2_Raw();
+	showTextRectangle("PM2", String(PM2), false);
+}
+
+
+void displayCO2() {
+	int CO2 = ag.getCO2_Raw();
+	showTextRectangle("CO2", String(CO2), false);
+}
+
+
+void displayTemp() {
+	TMP_RH result = ag.periodicFetchData();
+	float caliTemp = (result.t - TEMP_OFFSET);
+	if (FAHRENHEIT) {
+			showTextRectangle("TEMP", String((caliTemp * 1.8) + 32.0, 1) + "°F", true);
+	} else {
+			showTextRectangle("TEMP", String(caliTemp, 1) + "°C", true);
+	}
+}
+
+
+void displayHUM() {
+	TMP_RH result = ag.periodicFetchData();
+	showTextRectangle("HMTY", String(result.rh) + "%", false);
+}
+
+
+// display
+void showTextRectangle(String LN1, String LN2, boolean SMALL_TXT) {
+	display.clear();
+	display.setTextAlignment(TEXT_ALIGN_LEFT);
+	if (SMALL_TXT) {
+	display.setFont(ArialMT_Plain_16);
+	} else {
+	display.setFont(ArialMT_Plain_24);
+	}
+
+	display.drawString(32, 16, LN1);
+	display.drawString(32, 36, LN2);
+	display.display();
 }
--- README.md
+++ README.md
@@ -1,12 +1,15 @@
-# AirGradient Pushing to InfluxDB
+# Custom AirGradient Sketch
 
 [![Arduino Compile CI](../../actions/workflows/Arduino_CI.yml/badge.svg)](../../actions/workflows/Arduino_CI.yml)
 
-AirGradients sensor kit is quite good and reasonably affordable. This Arduino sketch allows you to integrated with a locally run InfluxDB instance. This was loosely based off of AirGradient original example with everything that wasn't required for the display removed and replaced with InfluxDB. There are a couple additional features which were added as well.
+AirGradients sensor kit is quite good and reasonably affordable. This Arduino sketch allows you to integrate with a locally run InfluxDB instance. This was loosely based off of AirGradient original example with everything that wasn't required for the display removed and replaced with InfluxDB. There are a couple additional features which were added as well.
+
+Note, InfluxDB isn't required there is a config option that will allow you to disable it.
 
 ## Modifications
 
-- Removed captive portal, Wi-Fi credentials are now defined below
+- removed captive portal, Wi-Fi credentials are now defined below
 - data is now sent to influxDB
 - added the ability to display Fahrenheit on display, celsius is still reported to the database
 - added a calibration offset for temperature (as Jeff Gerling noted temperatures are usually a bit high) the calibration value is subtracted from the reading
+- added the ability to update the database and the display independently
